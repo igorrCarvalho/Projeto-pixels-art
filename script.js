fa49716b564e06c26aa1dc03cf0b84536d828867
Igor Carvalho <igorr.ackerman@gmail.com>
2022-08-03 12:56:28 -0300

    Cria uma função para preencher um pixel do quadro com a cor selecionada (Requisitos 8 e 9)

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-function createPixels() {
-    let pixelBoard = document.querySelector('#pixel-board');
-    for (let index = 0; index < 25; index += 1) {
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        pixelBoard.appendChild(pixel);
-    }
-}
-createPixels();
-
-function firstColor() {
+window.onload = function() {
     const black = document.querySelector('.color');
-    black.style.backgroundColor = 'black';
-    black.className = 'color selected';
+    black.classList.add('black')
+    black.classList.add('selected')
+    
+    function createPixels() {
+        let pixelBoard = document.querySelector('#pixel-board');
+        for (let index = 0; index < 25; index += 1) {
+            let pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            pixelBoard.appendChild(pixel);
+        }
+    }
+    createPixels();
+
+    function colorSelect() {
+        const colors = document.querySelectorAll('.color');
+        for (let index = 0; index < colors.length; index += 1) {
+            colors[index].addEventListener('click', function(color) {
+                let colorChoosed = document.querySelector('.selected');
+                colorChoosed.classList.remove('selected');
+                color.target.classList.add('selected')
+            })
+        }
+    }
+    colorSelect();
+    
+    function applyColor() {
+        const bigPixel = document.querySelector('#pixel-board');
+        bigPixel.addEventListener('click', function(pixel) {
+            let colorSelected = document.querySelector('.selected')
+            let color = colorSelected.classList[1];
+            pixel.target.id = color;
+        })
+    }
+    applyColor();
 }
 
-firstColor();
+
